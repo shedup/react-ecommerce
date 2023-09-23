930b9f6bd93d0fb99c1c3202bae4d3ebc19e1e0f
Tenzin <chaksamt@gmail.com>
2023-09-23 00:24:49 -0500

    strip version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 const stripeServerSide = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2022-08-01",
+  apiVersion: "2023-08-16",
 });
 
 export { stripeServerSide };
